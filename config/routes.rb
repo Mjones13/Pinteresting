187cfc92dbb467bd679ebdddab3077069fb3af4a
Malcolm Jones <jonesmal@umich.edu>
2014-05-22 16:28:02 -0400

    Implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
@@ -55,4 +56,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
